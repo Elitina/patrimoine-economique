e8f6fa8775356993c1aadf6d4f0096c954edef9b
Elitina <hei.elitina@gmail.com>
2024-07-27 09:51:50 +0300

    Patrimoine

--- models/Possession.js
+++ models/Possession.js
@@ -1,12 +1,17 @@
-export default class Possession {
-  constructor(possesseur, libelle, valeur, date) {
-    this.possesseur = possesseur;
-    this.libelle = libelle;
-    this.valeur = valeur;
-    this.date = date;
+class Possession {
+  constructor(owner, acquisitionDate, lossDate, value, depreciation) {
+    this.owner = owner;
+    this.acquisitionDate = acquisitionDate;
+    this.lossDate = lossDate;
+    this.value = value;
+    this.depreciation = depreciation;
   }
 
-  getValeur(date) {
-    return this.valeur;
+  valueAfterDepreciation(checkingDate){
+    let depreciationPerDay = this.depreciation / 36500
+    let timeDiff = Math.abs(checkingDate.getTime() - this.acquisitionDate.getTime());
+    let depreciationValue = timeDiff * depreciationPerDay * this.value
+    let newValue = this.value - depreciationValue
+    return newValue
   }
 }
